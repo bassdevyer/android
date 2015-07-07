331e7ff3dd25ed7123eb4752ad33219f9d4c1a3b
miguetv17 <miguetv17@gmail.com>
2015-07-07 18:24:23 -0500

    Styling Row Items

--- SpotifyStreamer/app/src/main/java/ec/torres/spotifystreamer/TopActivity.java
+++ SpotifyStreamer/app/src/main/java/ec/torres/spotifystreamer/TopActivity.java
@@ -1,12 +1,12 @@
 package ec.torres.spotifystreamer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TopActivity extends ActionBarActivity {
+public class TopActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
